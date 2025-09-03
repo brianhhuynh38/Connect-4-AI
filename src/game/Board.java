e76c341aa44ba71203610a636aac6f791cd262f1
bhhuynh <brianhhuynh38@gmail.com>
2025-09-03 02:57:19 -0400

    Started on implementing board, but it might be easier to go with a simpler implementation

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,5 +1,27 @@
 package game;
 
 public class Board {
+
+    // Default dimensions for the Connect 4 Board
+    static final int DEFAULT_ROWS = 6;
+    static final int DEFAULT_COLUMNS = 7;
     
+    CheckerNode[][] layout;
+
+    
+    /**
+     * Default constructor that intializes the Board layout with the dimensions of a
+     * regular, real-life Connect 4 Board (6 rows, 7 columns)
+     */
+    public Board() {
+        this.layout = new CheckerNode[DEFAULT_ROWS][DEFAULT_COLUMNS];
+        for (CheckerNode[] row : layout) {
+            for (CheckerNode checker : row) {
+                //checker.setNearestNodes(neighbors);
+            }
+        }
+    }
+
+
+
 }
